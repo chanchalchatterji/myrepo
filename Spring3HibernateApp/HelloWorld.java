e3a841377da14fd4a520d93de27c5c05926958c7
EC2 Default User <ec2-user@ip-172-31-50-33.ec2.internal>
2017-05-25 07:44:12 +0000

    Modified HelloWord.java for scenario 1

--- Spring3HibernateApp/HelloWorld.java
+++ Spring3HibernateApp/HelloWorld.java
@@ -1,4 +1,4 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
+        System.out.println("Scenario 1: Build Hello World using Jenkins"); 
     }
